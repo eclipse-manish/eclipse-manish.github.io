af7338041b5fa3df8d8b2129951fd63601e2dc0d
Manish ✨ <143704410+eclipse-manish@users.noreply.github.com>
2023-11-03 19:16:55 +0530

    ~

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,8 @@
  * @description root Script File for My domain.
  */
 
+const root = document.documentElement;
+const head = document.head;
+const body = document.body;
+const css = document.querySelector("link").getAttribute("href");
+
